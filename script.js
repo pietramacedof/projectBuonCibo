c3773e87bec5a37beb8cc5b3bbd803219529e4c5
Pietra Geovanna M. Ferreira <pietrag89@gmail.com>
2024-02-09 00:22:10 -0300

    Project Buon Cibo

---
+++ script.js
@@ -0,0 +1,29 @@
+function mostrarsenha(){
+    var inputPass = document.getElementById('password');
+    var btnShowPass = document.getElementById('btn-senha');
+
+    if (inputPass.type === 'password'){
+        inputPass.setAttribute('type', 'text');
+        btnShowPass.classList.replace('bi-eye', 'bi-eye-slash');
+    }
+
+    else {
+        inputPass.setAttribute('type', 'password');
+        btnShowPass.classList.replace('bi-eye-slash', 'bi-eye');
+    }
+}
+
+function mostrarSenha() {
+    var inputPass = document.getElementById('password2');
+    var btnShowPass = document.getElementById('btn-senha2');
+    
+    if (inputPass.type === 'password') {
+    inputPass.setAttribute('type', 'text');
+    btnShowPass.classList.replace('bi-eye', 'bi-eye-slash');
+    } else {
+    inputPass.setAttribute('type', 'password');
+    btnShowPass.classList.replace('bi-eye-slash', 'bi-eye');
+    }
+    }
+
+    
